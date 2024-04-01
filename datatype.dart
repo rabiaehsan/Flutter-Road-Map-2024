a3f48a723a7d019ee701b452838942f53f69000c
rabiaehsan <44220680+rabiaehsan@users.noreply.github.com>
2024-04-01 01:10:32 -1200

    first commit

---
+++ datatype.dart
@@ -0,0 +1,30 @@
+void main() {
+  // var a = 10;
+  // var b = 20;
+  // var c = a + b;
+  // print(c);
+
+  // data types
+// integer
+  int a = 10;
+  int b = 20;
+  int c = a + b;
+  print(c);
+// Strings
+  String name = 'jhon';
+   print(name);
+// boolean
+  bool isTrue = true;
+  bool isFalse = false;
+  print(isTrue);
+  print(isFalse);
+
+// double
+  double d = 10.5;
+  double e = 20.5;
+  double f = d + e;
+  print(f);
+
+
+}
+
