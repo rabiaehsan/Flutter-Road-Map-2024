a626baabe5240c85f7cbfef0518eb95f8ef263de
rabiaehsan <44220680+rabiaehsan@users.noreply.github.com>
2024-04-02 00:56:27 -1200

    Arithmetic Operators

---
+++ Arithmetic Operators.dart
@@ -0,0 +1,42 @@
+// Arithmetic Operators
+void main(){
+  // +
+  int a = 10;
+  int b = 20;
+  int c = a + b;
+  print(c);
+  // -
+
+  int d = 10;
+  int e = 20;
+  int f = d - e;
+  print(f);
+
+  // unaryMinus
+
+  int g = 10;
+  int h = -g;
+  print(h);
+
+  // * 
+
+  int i = 10;
+  int j = 20;
+  int k = i * j;
+  print(k);
+
+  // /
+  double l = 20;
+  double m = 20;
+  double n = l / m;
+  print(n);
+
+  // %
+
+  int o = 10;
+  int p = 3;
+  int q = o % p;
+  print(q);
+
+
+}
