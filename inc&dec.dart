9e0454bda3e3f0215ae32e8bd28f9eeba993a653
rabiaehsan <44220680+rabiaehsan@users.noreply.github.com>
2024-04-03 01:51:30 -1200

    increment and Decrement

---
+++ inc&dec.dart
@@ -0,0 +1,30 @@
+//increment and decrement opreators
+
+void main() {
+// //	Pre Increment
+//   var a = 10;
+//   // a = a+1
+//   var b = ++a;
+//   print("a : $a");
+//   print("b : $b");
+  // post incement
+  // var e = 10;
+  // var f =e++;
+  // print("e : $e");
+  // print("f : $f");
+ 
+// post  decrement
+  //var c = 10;
+  // // c = c-1
+  // var d = c--;
+  // print("c : $c");
+  // print("d : $d");  
+
+  // pre  decrement
+  var c = 10;
+  // c = 1-c
+  var d = --c;
+  print("c : $c");
+  print("d : $d"); 
+
+}
